86b8c00bd4aa8238ac6520e41a21a9a240c6a5b2
Andrew Listopadov <andrew.listopadov@gmail.com>
2019-01-31 17:10:13 +0300

    replaces karma with jest; moves intro.js types to dev dependencies;

--- src/app/modules/charts/components/charts/pie-chart/pie-chart.component.spec.ts
+++ src/app/modules/charts/components/charts/pie-chart/pie-chart.component.spec.ts
@@ -1,25 +1,29 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { PieChartComponent } from './pie-chart.component';
+import { PieChartComponent } from './pie-chart.component';
+import { ChartConfigService } from '../chart-config.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-// describe('ChartComponent', () => {
-//   let component: PieChartComponent;
-//   let fixture: ComponentFixture<PieChartComponent>;
+describe('ChartComponent', () => {
+  let component: PieChartComponent;
+  let fixture: ComponentFixture<PieChartComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ PieChartComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PieChartComponent],
+      providers: [ChartConfigService],
+      schemas: [NO_ERRORS_SCHEMA],
+    })
+      .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PieChartComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PieChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/modules/charts/pages/charts/charts.page.component.spec.ts
+++ src/app/modules/charts/pages/charts/charts.page.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChartsPageComponent } from './charts.page.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ChartsService } from '../../services/charts.service';
+import { createSpyObj } from 'jest-createspyobj';
 
 describe('ChartsComponent', () => {
   let component: ChartsPageComponent;
   let fixture: ComponentFixture<ChartsPageComponent>;
 
-  const ChartsServiceSpyObj = jasmine.createSpyObj('ChartsService', {
-    'getSunnyDaysForSPb': Promise.resolve([]),
-    'getSunnyDaysForMoscow': Promise.resolve([]),
-    'getSunnyDaysForSochi': Promise.resolve([]),
-  });
+  const ChartsServiceSpyObj = createSpyObj(ChartsService);
+  ChartsServiceSpyObj.getSunnyDaysForMoscow.mockReturnValue(Promise.resolve([]));
+  ChartsServiceSpyObj.getSunnyDaysForSPb.mockReturnValue(Promise.resolve([]));
+  ChartsServiceSpyObj.getSunnyDaysForSochi.mockReturnValue(Promise.resolve([]));
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      providers: [{ provide: ChartsService, useValue: ChartsServiceSpyObj }],
       declarations: [ChartsPageComponent],
+      providers: [{ provide: ChartsService, useValue: ChartsServiceSpyObj }],
       schemas: [NO_ERRORS_SCHEMA],
     })
       .compileComponents();
